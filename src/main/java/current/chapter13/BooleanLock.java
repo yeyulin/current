8799be9a72e6f287e5274963bdb966c92754f3d9
Administrator <Administrator@NRD00RFFCZ5VNQ9>
2018-10-06 15:29:06 +0800

    自定义锁

---
+++ src/main/java/current/chapter13/BooleanLock.java
@@ -0,0 +1,121 @@
+package current.chapter13;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+ interface Lock {
+	 class TimeOutException extends Exception {
+
+	        public TimeOutException(String message) {
+	            super(message);
+	        }
+	    }
+
+	    void lock() throws InterruptedException;
+
+	    void lock(long mills) throws InterruptedException, TimeOutException;
+
+	    void unlock();
+
+	    Collection<Thread> getBlockedThread();
+
+	    int getBlockedSize();
+}
+public class BooleanLock implements Lock {
+
+    //The initValue is true indicated the lock have be get.
+    //The initValue is false indicated the lock is free (other thread can get this.)
+    private boolean initValue;
+
+    private Collection<Thread> blockedThreadCollection = new ArrayList<>();
+
+    private Thread currentThread;
+
+    public BooleanLock() {
+        this.initValue = false;
+    }
+
+    @Override
+    public synchronized void lock() throws InterruptedException {
+        while (initValue) {
+            blockedThreadCollection.add(Thread.currentThread());
+            this.wait();
+        }
+
+        blockedThreadCollection.remove(Thread.currentThread());
+        this.initValue = true;
+        this.currentThread = Thread.currentThread();
+    }
+
+    @Override
+    public synchronized void lock(long mills) throws InterruptedException, TimeOutException {
+        if (mills <= 0)
+            lock();
+
+        long hasRemaining = mills;
+        long endTime = System.currentTimeMillis() + mills;
+        while (initValue) {
+            if (hasRemaining <= 0)
+                throw new TimeOutException("Time out");
+            blockedThreadCollection.add(Thread.currentThread());
+            this.wait(mills);
+            hasRemaining = endTime - System.currentTimeMillis();
+        }
+
+        this.initValue = true;
+        this.currentThread = Thread.currentThread();
+
+    }
+
+    @Override
+    public synchronized void unlock() {
+        if (Thread.currentThread() == currentThread) {
+            this.initValue = false;
+            Optional.of(Thread.currentThread().getName() + " release the lock monitor.")
+                    .ifPresent(System.out::println);
+            this.notifyAll();
+        }
+    }
+
+    @Override
+    public Collection<Thread> getBlockedThread() {
+        return Collections.unmodifiableCollection(blockedThreadCollection);
+    }
+
+    @Override
+    public int getBlockedSize() {
+        return blockedThreadCollection.size();
+    }
+    
+    public static void main(String[] args) throws InterruptedException {
+
+        final BooleanLock booleanLock = new BooleanLock();
+        Stream.of("T1", "T2", "T3", "T4")
+                .forEach(name ->
+                        new Thread(() -> {
+                            try {
+                                booleanLock.lock(100L);
+                                Optional.of(Thread.currentThread().getName() + " have the lock Monitor")
+                                        .ifPresent(System.out::println);
+                                work();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            } catch (Lock.TimeOutException e) {
+                                Optional.of(Thread.currentThread().getName() + " time out")
+                                        .ifPresent(System.out::println);
+                            } finally {
+                                booleanLock.unlock();
+                            }
+                        }, name).start()
+                );
+    }
+
+    private static void work() throws InterruptedException {
+        Optional.of(Thread.currentThread().getName() + " is Working...")
+                .ifPresent(System.out::println);
+        Thread.sleep(40_000);
+    }
+}
